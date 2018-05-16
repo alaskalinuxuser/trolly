13adbe7d9945669b541e488f05c99f13eb36e5de
f4263 <fulvio.russo@guest.italiaonline.it>
2018-05-16 15:20:42 +0200

    merge

--- build/CMakeFiles/trolly.desktop.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/trolly.desktop"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/trolly.desktop.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/po/CMakeFiles/fulvio.trolly.pot.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/fulvio.trolly.pot"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/fulvio.trolly.pot.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
